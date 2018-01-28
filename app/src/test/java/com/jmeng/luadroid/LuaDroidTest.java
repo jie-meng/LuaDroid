eab3f7d66cf93a6fd767aa6bc97de87d1c8719fa
Jie Meng <jiemeng@thoughtworks.com>
2018-01-28 13:00:48 +0800

    Update sdk version

---
+++ app/src/test/java/com/jmeng/luadroid/LuaDroidTest.java
@@ -0,0 +1,16 @@
+package com.jmeng.luadroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jiemeng on 28/01/2018.
+ */
+
+public class LuaDroidTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
